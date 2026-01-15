55974498955b5af83e395e616b891f457526db96
Bartlomiej Kuzma <159695537+8ARTEZZIO@users.noreply.github.com>
2026-01-15 00:51:39 +0000

    Add plurality voting functionality in plurality.c
    Implement a plurality voting system with candidate management and vote counting.

---
+++ week3/problem_set3/plurality.c
@@ -0,0 +1,102 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+} candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // let's assume initially that the 0 index stands for the highest value
+    int max_ind = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > candidates[max_ind].votes)
+        {
+            max_ind = i;
+        }
+    }
+    // print the rest of ther winners if the votes are equal
+    for (int j = 0; j < candidate_count; j++)
+    {
+        if (candidates[j].votes == candidates[max_ind].votes && j != max_ind)
+        {
+            printf("%s\n", candidates[j].name);
+        }
+    }
+    // print the last highest candidate
+    printf("%s\n", candidates[max_ind].name);
+}
